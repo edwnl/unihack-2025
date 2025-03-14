2de804acad7a182e58c1570673f00e45f0e8eb4a
ed <edwinli123456@gmail.com>
2025-03-14 18:10:03 +1100

    init with https://start.spring.io/

---
+++ backend/src/main/java/com/edwnl/unihack/UnihackApplication.java
@@ -0,0 +1,13 @@
+package com.edwnl.unihack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnihackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnihackApplication.class, args);
+	}
+
+}
