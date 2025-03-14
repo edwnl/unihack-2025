366a1ecff754f1bc1086a36f0f1eeb60e673bdd8
FinnyEwe <finianwhewe@gmail.com>
2025-03-15 00:25:31 +1100

    make types for frontend

---
+++ frontend/lib/types.ts
@@ -0,0 +1,59 @@
+// generate types to use for frontend
+export type CardType = {
+  suit: string;
+  rank: string;
+};
+
+export type PlayerHandType = {
+  cards: CardType[];
+};
+
+export type PlayerType = {
+  id: string;
+  name: string;
+  online: boolean;
+  visuallyImpaired: boolean;
+  chips: number;
+  folded: boolean;
+  active: boolean;
+  hand?: PlayerHandType;
+  handRanking?: string;
+  lastAction?: string;
+  lastActionAmount?: number;
+};
+
+export type GameActionType = {
+  playerId?: string;
+  playerName?: string;
+  type: string;
+  amount?: number;
+  timestamp?: string;
+  card?: CardType;
+  message?: string;
+};
+
+export type GameRoomType = {
+  id: string;
+  players: PlayerType[];
+  gameState:
+    | "WAITING"
+    | "STARTED"
+    | "PREFLOP"
+    | "FLOP"
+    | "TURN"
+    | "RIVER"
+    | "SHOWDOWN"
+    | "ENDED";
+  waitingForCards: boolean;
+  communityCards: CardType[];
+  currentPlayerIndex: number;
+  pot: number;
+  actions?: GameActionType[];
+  bets?: Record<string, number>;
+  currentBet?: number;
+  smallBlindPosition: number;
+  dealerId?: string;
+  winnerIds?: string[];
+};
+
+export type UserRole = "PLAYER" | "DEALER" | "SCANNER";
