c8641383a77908581d39b0c444d982eb9e721f86
FinnyEwe <finianwhewe@gmail.com>
2025-03-14 23:39:03 +1100

    player statuses for settings and during game

---
+++ frontend/lib/types.ts
@@ -0,0 +1,11 @@
+// frontend/lib/types.ts
+export type PlayerType = {
+    id: string;
+    name: string;
+    online: boolean;
+    visuallyImpaired: boolean;
+    chips: number;
+    folded: boolean;
+    active: boolean;
+};
+
